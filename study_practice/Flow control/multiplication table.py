9ce7cdcfc8c00e6ddc75a87a13fec6ef879eaa57
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-04-17 21:19:28 +0900

    line update

--- study_practice/Flow control/multiplication table.py
+++ study_practice/Flow control/multiplication table.py
@@ -1,7 +1,13 @@
 # 구구단 작성
 
+
+num = 0
+
 # 2, 4, 6, 8 짝수 단을 작성
 for num in range(2, 10):
     for dan in range(1, 10):
         if num % 2 == 0:
             print(f"{num} X {dan} = {num * dan}")
+            
+    if num % 2 == 0:
+        print("=================")
