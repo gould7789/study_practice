de9214cfc7dfa336e24e53ec86060fd5f6282a9f
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-04-18 11:43:04 +0900

    for-range_practice

---
+++ study_practice/Flow control/Loop_practice_2.py
@@ -0,0 +1,17 @@
+for value in range(2):
+    print(value)    # 0, 1
+
+for value in range(3):
+    print(value)    # 0, 1, 2
+
+for value in range(1, 2):
+    print(value)    # 1
+
+for value in range(1, 3):
+    print(value)    # 1, 2
+
+for value in range(1, 11, 3):
+    print(value)    # 1, 4, 7, 10
+
+for value in range(10, 0, -3):
+    print(value)    # 10, 7, 4, 1
