bddde7d0edd46cb128acb34b3ca9e5968d93207c
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-04-18 13:47:24 +0900

    for-multiplication_practice

---
+++ study_practice/Flow control/Loop_practice_3.py
@@ -0,0 +1,12 @@
+# for count in range(3, -2, -1):
+#     print((count), end="")
+
+
+# 구구단
+for dan in range(2, 10, 2):
+    print(f"{dan}단 시작")
+    for num in range(1, 10):
+        print(f"{dan} x {num} = {dan * num}")
+
+    print("============")
+    
