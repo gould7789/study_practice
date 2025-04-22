5fa44c323d0365b206726420d7c05dad0fe8f688
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-04-22 16:24:24 +0900

    break practice

---
+++ study_practice/Flow control/break_flow.py
@@ -0,0 +1,24 @@
+for value_1 in range(2):
+    for value_2 in range(2):
+        for value_3 in range(2):
+            if value_2 == 1:
+                break
+            print(f"{value_1} {value_2} {value_3}\t", end="")
+            
+            
+        print()
+    print()
+    
+    
+# for value_1 in range(2):
+#     for value_2 in range(2):
+#         if value_2 == 1:
+#             break
+        
+#         for value_3 in range(2):
+            
+#             print(f"{value_1} {value_2} {value_3}\t", end="")
+            
+            
+#         print()
+#     print()
