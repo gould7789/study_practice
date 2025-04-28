87ae5d62ef177ad366a92c75f980c46a82ed54d8
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-04-28 10:38:15 +0900

    update

--- study_practice/exam_test/midterm_main.py
+++ study_practice/exam_test/midterm_main.py
@@ -9,39 +9,39 @@ while True:
 3. 지수 계산 (밑수 [정수], 지수 [정수] 입력)
 4. 종료""")
 
-# 사용자가 메뉴를 선택
+    # 사용자가 메뉴를 선택
     try:
         user_choice = int(input("메뉴를 선택하세요: "))
 
 
-    # 1번 메뉴를 선택했을 때
+        # 1번 메뉴를 선택했을 때
         if user_choice == 1:
             menu1_input1 = int(input("첫 번째 정수를 입력하세요: "))        # 사용자에게 첫 번째 정수 입력 받음
             menu1_input2 = int(input("두 번째 점수를 입력하세요: "))        # 사용자에게 두 번째 정수 입력 받음
             result = menu1_input1 + menu1_input2                          # 두 정수의 합 출력
             print(f"합계: {result}\n")
 
-    # 2번 메뉴를 선택했을 때
+        # 2번 메뉴를 선택했을 때
         elif user_choice == 2:
-            menu2_input1 = float(input("첫 번째 실수를 입력하세요: "))      # 사용자에게 첫 번째 실수 입력 받음
-            menu2_input2 = float(input("두 번째 실수를 입력하세요: "))      # 사용자에게 두 번째 실수 입력 받음
-            menu2_input3 = float(input("세 번째 실수를 입력하세요: "))      # 사용자에게 세 번째 실수 입력 받음
-            result = (menu2_input1 + menu2_input2 + menu2_input3) / 3     # 세 실수의 평균 출력
+            menu_input1 = float(input("첫 번째 실수를 입력하세요: "))      # 사용자에게 첫 번째 실수 입력 받음
+            menu_input2 = float(input("두 번째 실수를 입력하세요: "))      # 사용자에게 두 번째 실수 입력 받음
+            menu_input3 = float(input("세 번째 실수를 입력하세요: "))      # 사용자에게 세 번째 실수 입력 받음
+            result = (menu_input1 + menu_input2 + menu_input3) / 3     # 세 실수의 평균 출력
             print(f"평균: {result:.2f}\n")
 
-    # 3번 메뉴를 선택했을 때
+        # 3번 메뉴를 선택했을 때
         elif user_choice == 3:
-            menu3_input1 = int(input("밑수를 입력하세요: "))                # 사용자에게 밑수로 사용할 정수 입력 받음
-            menu3_input2 = int(input("지수를 입력하세요: "))                # 사용자에게 지수로 사용할 정수 입력 받음
-            result = menu3_input1 ** menu3_input2                          # 지수 계산 결과 출력
+            menu_input1 = int(input("밑수를 입력하세요: "))                # 사용자에게 밑수로 사용할 정수 입력 받음
+            menu_input2 = int(input("지수를 입력하세요: "))                # 사용자에게 지수로 사용할 정수 입력 받음
+            result = menu_input1 ** menu_input2                          # 지수 계산 결과 출력
             print(f"결과: {result}\n")
 
-    # 4번 메뉴를 선택했을 때
+        # 4번 메뉴를 선택했을 때
         elif user_choice == 4:                                             
             print("프로그램 종료")                                          # 사용자가 4번 메뉴 선택시 프로그램 종료
             break
 
-    # 사용자가 '1~4' 이외의 메뉴를 선택했을 때
+        # 사용자가 '1~4' 이외의 메뉴를 선택했을 때
         else:
             print("잘못된 입력입니다.\n")
             
