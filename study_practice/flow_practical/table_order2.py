d95d8e0c9fc7904bba3772ae1921a564ab3ba31b
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-02 11:42:08 +0900

    number table

---
+++ study_practice/flow_practical/table_order2.py
@@ -0,0 +1,40 @@
+# 1 버전과 동일하게 T, M, N을 입력받고, 출력 옵션을 선택하여 출력
+# 랜덤 함수 사용
+import random
+
+input_t = int(input("테이블 개수 입력: "))  # 사용자에게서 출력할 테이블 갯수를 입력 받음
+input_m = int(input("행 수 입력: "))        # 사용자에게서 출력할 행의 갯수를 입력 받음
+input_n = int(input("열 수 입력: "))        # 사용자에게서 출력할 열의 갯수를 입력 받음
+
+# 메뉴 출력
+print("""출력 옵션을 선택하세요: 
+1. 1부터 시작하여 열 방향으로 증가
+2. 1~100 사이 랜덤 값 출력""")
+
+# 사용자가 메뉴 입력
+menu = int(input("옵션 (1 또는 2): "))
+print()
+
+# 1번 메뉴를 선택했을 때 순자 숫자 출력 : 1부터 시작하여 열 방향으로 증가
+if menu == 1:
+    for t in range(1, input_t + 1):
+        print(f"테이블 {t}")
+        count = 0
+        
+        for _ in range(input_m):
+            for _ in range(input_n):
+                count += 1
+                print(count, end="\t")
+            print()
+        print()
+
+# 2번 메뉴를 선택했을 때 랜덤 숫자 출력: 1~100 사이 난수 채우기
+elif menu == 2:
+    for t in range(1, input_t + 1):
+        print(f"테이블 {t}")
+        
+        for _ in range(input_m):
+            for _ in range(input_n):
+                print(random.randint(1, 100), end=" ")
+            print()
+        print()
