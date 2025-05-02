98740924bced748e3f36ea1f1fc98718eaeb30e4
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-02 10:38:06 +0900

    star table

---
+++ study_practice/flow_practical/table_order.py
@@ -0,0 +1,13 @@
+# 사용자로부터 테이블 개수 T
+# 각 테이블의 행 수 M
+# 열 수 N을 입력 받아 *로 구성된 MxN 테이블을 T개 출력하라
+
+input_t = int(input("테이블 개수 입력: "))
+input_m = int(input("행 수 입력: "))
+input_n = int(input("열 수 입력: "))
+
+for _ in range(input_t):
+    for _ in range(1, input_m + 1):
+        print("*" * input_n)
+        
+    print()
