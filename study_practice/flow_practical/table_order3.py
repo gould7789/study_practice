f62aecd2e354edcc9b826fc521d8e8e5683e6ea5
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-08 20:47:58 +0900

    menu 2

--- study_practice/flow_practical/table_order3.py
+++ study_practice/flow_practical/table_order3.py
@@ -1,11 +1,16 @@
 # 다른 옵션
 """
-1. 테두리
-2. 왼쪽-> 오른쪽 대각선
-3. 오른쪽 -> 왼쪽 대각선
+1. 왼쪽-> 오른쪽 대각선
+2. 오른쪽 -> 왼쪽 대각선
+3. 테두리
 4. 양방향 대각선
 """
 
+"""
+*  
+ *
+  *
+"""
 row = 3
 col = 3
 for row_num in range(row):
@@ -15,4 +20,19 @@ for row_num in range(row):
         # else:
         #     print(" ", end="")
         print("*" if row_num == col_num else " ", end= "")
+    print()
+    
+    
+"""
+  *
+ *
+*
+"""
+for row_num in range(row):
+    for col_num in range(col):
+        if row_num + col_num == col - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+        
     print()
