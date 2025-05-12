b29f81957c47238e2af0800f59aa7270f5417303
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-12 12:51:59 +0900

    list practice

--- study_practice/Data_Structure/list_practice.py
+++ study_practice/Data_Structure/list_practice.py
@@ -39,3 +39,28 @@ print(bar.pop(0))
 print(bar)
 print(bar.pop(2))
 print(bar)
+
+# 원시 변수와 참조 변수의 관계
+bar = [10, 20, 30]  # bar은 참조 변수
+
+bar[0] = 20
+
+foo = bar[0]    # foo는 원시 변수
+foo = 30        # 서로 영향 x
+print(bar)      # [20, 20, 30]
+
+
+# 삭제
+bar = [num for num in range(1, 11)]
+print(bar)
+
+del bar[3]
+print(bar)
+
+bar.pop(2)
+print(bar)
+
+bar.remove(8)
+print(bar)
+
+bar.remove(10)
