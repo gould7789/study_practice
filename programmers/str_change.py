3d52e3355f3f6115c506a06eb728c1d58a5f7c7a
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-07-05 19:34:13 +0900

    slicing ver

--- programmers/str_change.py
+++ programmers/str_change.py
@@ -3,7 +3,7 @@
 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요
 """
-
+# vanilla
 def solution(my_string, overwrite_string, s):
     answer = ""
     word = "".join(my_string)
@@ -18,13 +18,9 @@ def solution(my_string, overwrite_string, s):
     return answer
 
 
-print(solution("He11oWor1d", "lloWorl", 2)) # "HelloWorld"
-print(solution("Program29b8UYP", "merS123", 7)) # ProgrammerS123     13개 7개
+# 슬라이싱 버전
+def solution(my_string, overwrite_string, s):
+    return my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
 
-
-
-
-# a = "hello"
-# b = "".join(a)
-# c = list(b)
-# print(c)
+print(solution("He11oWor1d", "lloWorl", 2))     # "HelloWorld"
+print(solution("Program29b8UYP", "merS123", 7)) # ProgrammerS123     
