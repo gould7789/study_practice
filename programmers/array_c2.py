51c11518b76d138c792093d708ccd2e7d8053428
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-08-04 16:22:17 +0900

    created array

---
+++ programmers/array_c2.py
@@ -0,0 +1,20 @@
+"""
+문자열 배열 intStrs와 정수 k, s, l가 주어집니다. 
+intStrs의 원소는 숫자로 이루어져 있습니다.
+
+배열 intStrs의 각 원소마다 s번 인덱스에서 시작하는 길이 l짜리 부분 문자열을 잘라내 정수로 변환합니다. 
+이때 변환한 정수값이 k보다 큰 값들을 담은 배열을 return 하는 solution 함수를 완성해 주세요.
+"""
+
+def solution(intStrs, k, s, l):
+    answer = []
+    
+    for num in intStrs:
+        s_num = int(num[s:s+l])     # 정수인 k와 비교를 위해 정수로 타입변환
+        # 비교 후 더 큰 숫자일 때 리스트에 추가
+        if s_num > k:
+            answer.append(s_num)
+    
+    return answer
+
+print(solution(["0123456789","9876543210","9999999999999"], 50000, 5, 5))   # [56789, 99999]
