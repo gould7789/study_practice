5815e7a16d6ad295350587d7ec4cc2c9bdd34dc2
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-08-06 13:16:19 +0900

    suffix check

---
+++ programmers/suffix2.py
@@ -0,0 +1,23 @@
+"""
+문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다.
+myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 
+pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요
+"""
+
+def solution(myString, pat):
+    answer = ''
+    
+    for word in myString:
+        if word == "A":
+            word = "B"
+            answer += word
+        else:
+            word = "A"
+            answer += word
+    
+    suffix = [answer[n:] for n in range(len(answer))]
+    
+    return 1 if pat in suffix else 0
+
+print(solution("ABBAA", "AABB"))    # 1
+print(solution("ABAB", "ABAB"))     # 0
