4d2244bcb7af34048565a96be6f987e94212c954
gould7789 <yukif029@g.yju.ac.kr>
2025-08-14 20:38:14 +0900

    2_dimensional list

--- programmers/2_dimensional_list.py
+++ programmers/2_dimensional_list.py
@@ -5,20 +5,20 @@
 # n의 갯수에 맞게 배열을 넣고, n에 맞게 0을 넣은 뒤, 순서에 맞게 1을 넣는다
 
 def solution(n):
-    # answer = []
-    # # n개의 리스트 삽입
-    # for num in range(n):
-    #     input_list = [] # 1차원 리스트 작성
+    answer = []
+    # n개의 리스트 삽입
+    for num in range(n):
+        input_list = [] # 1차원 리스트 작성
 
-    #     # 리스트 안의 1 또는 0 삽입
-    #     for num2 in range(n):
-    #         input_list.append(0)
+        # 리스트 안의 1 또는 0 삽입
+        for _ in range(n):
+            input_list.append(0)
 
-    #     # 인덱스 순서에 맞게 1 삽입
-    #     input_list[num] = 1
+        # 인덱스 순서에 맞게 1 삽입
+        input_list[num] = 1
 
-    #     # 완성된 리스트를 최종 결과 리스트에 삽입
-    #     answer.append(input_list)
+        # 완성된 리스트를 최종 결과 리스트에 삽입
+        answer.append(input_list)
     
 
     # 리스트 컴프리핸션
